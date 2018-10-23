10a9543cdae99a23446d168c405a6b9f780937d0
Louis <louis-efflam@hotmail.fr>
2018-10-23 18:32:15 -0500

    add data-category

--- TP3/assets/scripts/controllers/products.js
+++ TP3/assets/scripts/controllers/products.js
@@ -1,7 +1,7 @@
 let products;
 getProducts().then((productsJson) => {
-  products = productsJson;
-  products.forEach(product => $('#products-list').append(`<div class="product">
+    products = productsJson;
+    products.forEach(product => $('#products-list').append(`<div class="product">
     <a href="./product.html" title="En savoir plus...">
       <h2>${product.name}</h2>
       <img alt="${product.name}" src="./assets/img/${product.image}">
@@ -10,6 +10,26 @@ getProducts().then((productsJson) => {
   </div>`));
 });
 
-$('#product-categories').children().on('click', () => {
-  console.log(products);
+
+let productsByCategory = (category) => {
+    $('#products-list').empty();
+    products.forEach(product => {
+            if (product.category === category) {
+                $('#products-list').append(`<div class="product">
+        <a href="./product.html" title="En savoir plus...">
+            <h2>${product.name}</h2>
+            <img alt="${product.name}" src="./assets/img/${product.image}">
+            <p class="price"><small>Prix</small> ${product.price}&thinsp;$</p>
+        </a>
+        </div>`);
+
+            }
+        }
+    );
+};
+
+
+$('#product-categories').children().click(e => {
+    let category = $(e.target).data("category");
+    productsByCategory(category)
 });
