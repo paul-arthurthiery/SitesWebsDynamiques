435812939d532add790eecbbae3f7c87e3aeca63
Paul-Arthur THIERY <paul-arthur.thiery@isep.fr>
2018-10-29 01:14:58 -0400

    sorting in shopping cart

--- TP3/assets/scripts/controllers/products.js
+++ TP3/assets/scripts/controllers/products.js
@@ -6,55 +6,55 @@ let firstLoad = true;
 
 
 const sortWithCriteria = (criteria) => {
-    let sortKey;
-    let reverseSort = false;
-    let priceSort = false;
-    switch (true) {
-        case (criteria === 'price'):
-            sortKey = '.raw-price';
-            priceSort = true;
-            break;
-        case (criteria === 'name'):
-            sortKey = 'h2';
-            break;
-        case (criteria === 'reversePrice'):
-            sortKey = '.raw-price';
-            reverseSort = true;
-            priceSort = true;
-            break;
-        case (criteria === 'reverseName'):
-            sortKey = 'h2';
-            reverseSort = true;
-            break;
-        default:
-            return false;
+  let sortKey;
+  let reverseSort = false;
+  let priceSort = false;
+  switch (true) {
+    case (criteria === 'price'):
+      sortKey = '.raw-price';
+      priceSort = true;
+      break;
+    case (criteria === 'name'):
+      sortKey = 'h2';
+      break;
+    case (criteria === 'reversePrice'):
+      sortKey = '.raw-price';
+      reverseSort = true;
+      priceSort = true;
+      break;
+    case (criteria === 'reverseName'):
+      sortKey = 'h2';
+      reverseSort = true;
+      break;
+    default:
+      return false;
+  }
+  const items = $('.products').children().sort((a, b) => {
+    let aValue = $(sortKey, a).text();
+    let bValue = $(sortKey, b).text();
+    if (priceSort) {
+      aValue = parseInt(aValue, 10);
+      bValue = parseInt(bValue, 10);
+    } else {
+      aValue = aValue.toUpperCase();
+      bValue = bValue.toUpperCase();
     }
-    const items = $('.products').children().sort((a, b) => {
-        let aValue = $(sortKey, a).text();
-        let bValue = $(sortKey, b).text();
-        if (priceSort) {
-            aValue = parseInt(aValue, 10);
-            bValue = parseInt(bValue, 10);
-        } else {
-            aValue = aValue.toUpperCase();
-            bValue = bValue.toUpperCase();
-        }
-        if (!reverseSort) {
-            if (aValue < bValue) return -1;
-            return aValue > bValue ? 1 : 0;
-        }
-        if (aValue < bValue) return 1;
-        return aValue > bValue ? -1 : 0;
-    });
-    $('.products').append(items);
-    return true;
+    if (!reverseSort) {
+      if (aValue < bValue) return -1;
+      return aValue > bValue ? 1 : 0;
+    }
+    if (aValue < bValue) return 1;
+    return aValue > bValue ? -1 : 0;
+  });
+  $('.products').append(items);
+  return true;
 };
 
 const refreshProducts = () => {
-    getProducts().then((productsJson) => {
-        $('#products-list').empty();
-        products = productsJson;
-        products.forEach(product => $('#products-list').append(`
+  getProducts().then((productsJson) => {
+    $('#products-list').empty();
+    products = productsJson;
+    products.forEach(product => $('#products-list').append(`
     <div class="product">
       <a href="./product.html?id=${product.id}" title="En savoir plus...">
         <h2>${product.name}</h2>
@@ -62,46 +62,46 @@ const refreshProducts = () => {
         <p class="price"><small>Prix</small> <span class="raw-price">${product.price}</span>&thinsp;$</p>
       </a>
     </div>`));
-        if (firstLoad) {
-            firstLoad = false;
-            sortWithCriteria('price');
-        }
-    });
+    if (firstLoad) {
+      firstLoad = false;
+      sortWithCriteria('price');
+    }
+  });
 };
 
 const productsByCategory = (category) => {
-    $('#products-list').empty();
-    let productCounter = 0;
-    products.forEach((product) => {
-        if (category === 'all') {
-            refreshProducts()
-        } else if (product.category === category) {
-            productCounter += 1;
-            $('#products-list').append(`<div class="product">
+  $('#products-list').empty();
+  let productCounter = 0;
+  products.forEach((product) => {
+    if (category === 'all') {
+      refreshProducts();
+    } else if (product.category === category) {
+      productCounter += 1;
+      $('#products-list').append(`<div class="product">
         <a href="./product.html?id=${product.id}" title="En savoir plus...">
             <h2>${product.name}</h2>
             <img alt="${product.name}" src="./assets/img/${product.image}">
             <p class="price"><small>Prix</small> ${product.price}&thinsp;$</p>
         </a>
         </div>`);
-        }
-    });
-    $('#products-count').text(`${productCounter} produits`);
+    }
+  });
+  $('#products-count').text(`${productCounter} produits`);
 };
 
 $('#product-categories').children().click((e) => {
-    const category = $(e.target).data('category');
-    productsByCategory(category);
-    const selected = $('#product-categories').children();
-    selected.removeClass('selected');
-    $(e.target).addClass('selected');
+  const category = $(e.target).data('category');
+  productsByCategory(category);
+  const selected = $('#product-categories').children();
+  selected.removeClass('selected');
+  $(e.target).addClass('selected');
 });
 
 $('#product-criteria').children().click((e) => {
-    sortWithCriteria($(e.target).data('criteria'));
-    const selected = $('#product-criteria').children();
-    selected.removeClass('selected');
-    $(e.target).addClass('selected');
+  sortWithCriteria($(e.target).data('criteria'));
+  const selected = $('#product-criteria').children();
+  selected.removeClass('selected');
+  $(e.target).addClass('selected');
 });
 
 refreshProducts();
--- TP3/assets/scripts/controllers/shopping-cart.js
+++ TP3/assets/scripts/controllers/shopping-cart.js
@@ -4,43 +4,67 @@ const createShoppingCart = async () => {
   const idarray = [];
   const panier = JSON.parse(localStorage.getItem('panier'));
   if (!panier || panier.length === 0) {
-    $('article').html('<h1 id="panier">Panier</h1> <p>Aucun produit dans votre panier</p>');
-  } else {
-    let price = 0;
-    panier.forEach((id) => {
-      const productOccurence = $.grep(idarray, elem => elem === id).length; // idarray.reduce((acc, curr) => curr===id ? acc+=1:acc, 0)
-      if (productOccurence === 0) {
-        idarray.push(id);
-        const numOccurences = $.grep(panier, elem => elem === id).length;
-        products.forEach((product) => {
-          if (product.id === parseInt(id, 10)) {
-            price += Math.round(numOccurences * parseFloat(product.price) * 100) / 100;
-            console.log(numOccurences * parseFloat(product.price));
-            $('#table-body').append(`
-              <tr>
-                <td><button title="Supprimer"><i class="fa fa-times"></i></button></td>
-                <td><a href="./product.html?id=${id}">${product.name}</a></td>
-                <td>${product.price}&thinsp;$</td>
-                <td>
-                  <div class="row">
-                    <div class="col">
-                      <button title="Retirer" disabled=""><i class="fa fa-minus"></i></button>
-                    </div>
-                    <div class="col">${numOccurences}</div>
-                    <div class="col">
-                      <button title="Ajouter"><i class="fa fa-plus"></i></button>
-                    </div>
-                  </div>
-                </td>
-                <td>${Math.round(numOccurences * parseFloat(product.price) * 100) / 100}&thinsp;$</td>
-              </tr>`);
-          }
-        });
-      }
-    });
-    $('#prixtotal').html(`Total: <strong>${price}$</strong>`);
+    $('article').append('<p>Aucun produit dans votre panier</p>');
+    return false;
   }
-};
+  $('article').append(`
+      <table class="table shopping-cart-table">
+        <thead>
+        <tr>
+            <th></th>
+            <th>Produit</th>
+            <th>Prix unitaire</th>
+            <th>Quantité</th>
+            <th>Prix</th>
+        </tr>
+        </thead>
+        <tbody id="table-body">
 
+        </tbody>
+    </table>
+    <p class="shopping-cart-total" id="prixtotal"></p>
+    <a class="btn pull-right" href="./order.html">Commander <i class="fa fa-angle-double-right"></i></a>
+    <button class="btn"><i class="fa fa-trash-o"></i>&nbsp; Vider le panier</button>`);
+  let price = 0;
+  panier.forEach((id) => {
+    const productOccurence = $.grep(idarray, elem => elem === id).length; // idarray.reduce((acc, curr) => curr===id ? acc+=1:acc, 0)
+    if (productOccurence === 0) {
+      idarray.push(id);
+      const numOccurences = $.grep(panier, elem => elem === id).length;
+      products.forEach((product) => {
+        if (product.id === parseInt(id, 10)) {
+          price += Math.round(numOccurences * parseFloat(product.price) * 100) / 100;
+          $('#table-body').append(`
+            <tr>
+              <td><button title="Supprimer"><i class="fa fa-times"></i></button></td>
+              <td><a href="./product.html?id=${id}" class="name">${product.name}</a></td>
+              <td>${product.price}&thinsp;$</td>
+              <td>
+                <div class="row">
+                  <div class="col">
+                    <button title="Retirer" disabled=""><i class="fa fa-minus"></i></button>
+                  </div>
+                  <div class="col">${numOccurences}</div>
+                  <div class="col">
+                    <button title="Ajouter"><i class="fa fa-plus"></i></button>
+                  </div>
+                </div>
+              </td>
+              <td>${Math.round(numOccurences * parseFloat(product.price) * 100) / 100}&thinsp;$</td>
+            </tr>`);
+        }
+      });
+    }
+  });
+  const sortedTable = $('#table-body').children().sort((a, b) => {
+    const aValue = $('.name', a).text().toUpperCase();
+    const bValue = $('.name', b).text().toUpperCase();
+    if (aValue < bValue) return -1;
+    return aValue > bValue ? 1 : 0;
+  });
+  $('#table-body').append(sortedTable);
+  $('#prixtotal').html(`Total: <strong>${price}$</strong>`);
+  return true;
+};
 
 createShoppingCart();
