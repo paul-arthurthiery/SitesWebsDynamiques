60d2ec733dffff3805d4649e0c4cbfa4240e60c8
Paul-Arthur THIERY <paul-arthur.thiery@isep.fr>
2018-10-28 23:12:38 -0400

    fixed empty shopping cart page

--- TP3/assets/scripts/controllers/shopping-cart.js
+++ TP3/assets/scripts/controllers/shopping-cart.js
@@ -1,66 +1,46 @@
-
-const createShoppingCart = () => {
-    getProducts().then((productsJson) => {
-        $('#table-body').empty();
-        let idarray = []
-        products = productsJson;
-        panier = JSON.parse(localStorage.getItem("panier"));
-        if (!panier || panier.length === 0) {
-            $("main").empty();
-            $("main").append(' <h1>Panier</h1> ' +
-                '<p>Aucun produit dans votre panier</p>')
-        } else {
-            let price = 0
-            panier.forEach(id => {
-                var numoccur = $.grep(idarray, function (elem) {
-                    return elem === id;
-                }).length;
-
-                if (numoccur === 0) {
-                    idarray.push(id)
-
-                    var numOccurences = $.grep(panier, function (elem) {
-                        return elem === id;
-                    }).length;
-                    products.forEach(product => {
-
-                        if (product.id === parseInt(id)) {
-                            price = price + Math.round(numOccurences * parseFloat(product.price) * 100) / 100
-                            console.log(numOccurences * parseFloat(product.price))
-                            $('#table-body').append(`
-    <tr>
-            <td><button title="Supprimer"><i class="fa fa-times"></i></button></td>
-            <td><a href="./product.html?id=${id}">${product.name}</a></td>
-            <td>${product.price}&thinsp;$</td>
-            <td>
-              <div class="row">
-                <div class="col">
-                  <button title="Retirer" disabled=""><i class="fa fa-minus"></i></button>
-                </div>
-                <div class="col">${numOccurences}</div>
-                <div class="col">
-                  <button title="Ajouter"><i class="fa fa-plus"></i></button>
-                </div>
-              </div>
-            </td>
-            <td>${Math.round(numOccurences * parseFloat(product.price) * 100) / 100}&thinsp;$</td>
-          </tr>`)
-
-                        }
-
-
-                    })
-
-                }
-
-            })
-            $('#prixtotal').empty();
-            $('#prixtotal').append("Total: <strong>" + price + "$</strong>")
-            ;
-        }
-
-
+/* global getProducts $ localStorage */
+const createShoppingCart = async () => {
+  const products = await getProducts();
+  const idarray = [];
+  const panier = JSON.parse(localStorage.getItem('panier'));
+  if (!panier || panier.length === 0) {
+    $('article').html('<h1 id="panier">Panier</h1> <p>Aucun produit dans votre panier</p>');
+  } else {
+    let price = 0;
+    panier.forEach((id) => {
+      const productOccurence = $.grep(idarray, elem => elem === id).length; // idarray.reduce((acc, curr) => curr===id ? acc+=1:acc, 0)
+      if (productOccurence === 0) {
+        idarray.push(id);
+        const numOccurences = $.grep(panier, elem => elem === id).length;
+        products.forEach((product) => {
+          if (product.id === parseInt(id, 10)) {
+            price += Math.round(numOccurences * parseFloat(product.price) * 100) / 100;
+            console.log(numOccurences * parseFloat(product.price));
+            $('#table-body').append(`
+              <tr>
+                <td><button title="Supprimer"><i class="fa fa-times"></i></button></td>
+                <td><a href="./product.html?id=${id}">${product.name}</a></td>
+                <td>${product.price}&thinsp;$</td>
+                <td>
+                  <div class="row">
+                    <div class="col">
+                      <button title="Retirer" disabled=""><i class="fa fa-minus"></i></button>
+                    </div>
+                    <div class="col">${numOccurences}</div>
+                    <div class="col">
+                      <button title="Ajouter"><i class="fa fa-plus"></i></button>
+                    </div>
+                  </div>
+                </td>
+                <td>${Math.round(numOccurences * parseFloat(product.price) * 100) / 100}&thinsp;$</td>
+              </tr>`);
+          }
+        });
+      }
     });
+    $('#prixtotal').html(`Total: <strong>${price}$</strong>`);
+  }
 };
+
 
 createShoppingCart();
