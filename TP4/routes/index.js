520e9c82270d4b55cdd04fbca8360635a448c40e
Paul-Arthur THIERY <paul-arthur.thiery@isep.fr>
2018-11-06 18:14:54 -0500

    :sparkles: accueil and / in same route

--- TP4/routes/index.js
+++ TP4/routes/index.js
@@ -2,11 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Accueil',
-    message: 'Ça semble fonctionner!',
-  });
+router.get(['/', '/accueil'], (req, res) => {
+  res.render('index');
 });
 
 module.exports = router;
