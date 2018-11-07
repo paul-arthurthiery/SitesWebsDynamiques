b7fb5b17bc2784f6ed17a015a71f44ec8c22add4
Louis <louis-efflam@hotmail.fr>
2018-11-06 18:12:03 -0600

    update route

--- TP4/routes/index.js
+++ TP4/routes/index.js
@@ -2,11 +2,29 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Accueil',
-    message: 'Ça semble fonctionner!',
-  });
+router.get('/produits', (req, res) => {
+  res.render('products');
+});
+
+router.get('/produits/:id', (req, res) => {
+    var id = req.params.id;
+    res.render('products');
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+router.get('/panier', (req, res) => {
+    res.render('panier');
+});
+
+router.get('/commande', (req, res) => {
+    res.render('order');
+});
+
+router.get('/confirmation', (req, res) => {
+    res.render('confirmation');
 });
 
 module.exports = router;
